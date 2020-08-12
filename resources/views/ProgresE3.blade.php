f23d81fa538becb27291323af02145f4641a7600
Dwiky Rahman Hidayat <69495619+dewrchmn0@users.noreply.github.com>
2020-08-12 15:06:00 +0700

    Commit : DwikyRH, Do: UI Eselon 3, To do : Modal

---
+++ resources/views/ProgresE3.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <script src="js/jquery-3.4.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <style type="">
+        .margin{
+            margin: 0px 10px 10px 10px;
+        }
+    </style>
+    <style type="">
+        .margin2{
+            margin: 10px 10px 0px 10px;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+<br>
+    <div class="">
+        <div class="row">
+            <div class="col-sm-12">
+                <center>
+                    <h1>Eselon 3</h1>
+                </center>
+            </div>
+        </div>
+    </div>
+    <br>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <!-- <a class="navbar-brand" href="#">Logo</a> -->
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                    <a class="nav-link" href="Eslon3.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ToDoE3.php">To Do</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ProgresE3.php">Progres</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="SelesaiE3.php">Selesai</a>
+                </li>
+
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Dropdown</a>
+
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    </div>
+
+                </li> -->
+            </ul>
+        </nav>
+
+        
+        <div class="margin2">
+        <div class="row">
+            <div class="col-sm-12">
+                <center>
+                    <h1>Progres</h1>
+                </center>
+            </div>
+        </div>
+    </div>
+    <br>
+
+
+        <div class="container-fluit">
+        <div class="margin">
+        <div class="col-sm-12">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark" align="center">
+                <tr>
+                        <th>Kode</th>
+
+                        <th>Judul</th>
+                        
+                        
+                        
+                        <th>Note</th>
+                        <th>Kategori</th>
+                        <th>Date</th>
+                        <th>Aksi</th>
+                        <!-- <th>Harga Beli</th>
+                        <th>Ubah</th> -->
+                    </tr>
+                </thead>
+                <tbody align="center">
+
+                    
+                    <tr>
+                        <td>Kode</td>
+                        <!-- <td>Tugas</td> -->
+                        <!-- <td>To do</td> -->
+                        <td>Tugas</td>
+                        <td>ashdaksh</td>
+                        <td></td>
+                        <td>date</td>
+                        <td><a href="Ubah-barang.php?id=----belum---"><button type="button"
+                                    class=" btn btn-success">Selesai</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                            <!-- <a href="hapus.php?id=&status=Barang1"><button type="submit"
+                                    class=" btn btn-danger" onclick="Hapus()"><img src="silang.png" alt="" width="25"
+                                        height="25"></button></a> -->
+                                        </td>
+                    </tr>
+                    
+
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+
+</body>
+</html>
